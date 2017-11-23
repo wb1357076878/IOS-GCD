3d350b14e8d3960ae6a299cf08a61833fe08ff62
wangbo <wangbo793x@163.com>
2017-11-23 22:32:34 +0800

    first commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by WangBo on 2017/11/23.
+//  Copyright © 2017年 WangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by WangBo on 2017/11/23.
+//  Copyright © 2017年 WangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
