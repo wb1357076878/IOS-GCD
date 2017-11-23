3138beac5fa8c304da791be6e562347e733598e0
wangbo <wangbo793x@163.com>
2017-11-23 22:46:34 +0800

    GCD info

--- GCD/ViewController.m
+++ GCD/ViewController.m
@@ -16,7 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 1. 队列的创建 DISPATCH_QUEUE_SERIAL: 串行队列 DISPATCH_QUEUE_CONCURRENT: 并行队列
+    dispatch_queue_t queue1 = dispatch_queue_create("queue1", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t queue2 = dispatch_queue_create("queue2", DISPATCH_QUEUE_CONCURRENT);
+    
+    // 2. 任务的创建
+    dispatch_sync(queue1, ^{ // 同步执行任务，不会创建新的线程
+        NSLog(@"%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue2, ^{ // 异步执行任务，会创建新的线程
+        NSLog(@"%@",[NSThread currentThread]);
+    });
 }
 
 
